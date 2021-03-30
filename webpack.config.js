dc044d8e54327ac4cfc0a9f982a51c3127557d86
Jayesh <kr.jayesh94@gmail.com>
2021-03-30 13:26:03 +0530

    weback impl

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve("build"),
+    filename: "index.js",
+    libraryTarget: "commonjs2"
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ]
+  },
+  externals: {
+    react: "react"
+  }
+};
