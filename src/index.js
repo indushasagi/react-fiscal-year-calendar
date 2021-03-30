03a200a667a39d1d202cf453c36a64e760c24fde
Jayesh <kr.jayesh94@gmail.com>
2021-03-30 20:26:36 +0530

    code tweaks and stylings

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import FiscalCalendar from './components/Calendar';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+import FiscalCalendar from './components/FiscalCalendar';
 
-//export default FiscalCalendar;
-ReactDOM.render(
-  <React.StrictMode>
-    <FiscalCalendar year={2021} quarterMonth={'OCTOBER'}/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default FiscalCalendar;
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <FiscalCalendar year={2021} quarterMonth={'FEBRUARY'}/>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
