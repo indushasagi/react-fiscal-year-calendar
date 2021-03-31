cd9e07fbc5aff82aa089e4a2dac1eaec15e322e2
Jayesh <kr.jayesh94@gmail.com>
2021-03-31 18:05:03 +0530

    style tweaks

--- src/components/WeekHeader.js
+++ src/components/WeekHeader.js
@@ -5,9 +5,9 @@ import { WeekDetails } from "../utils/calendar-util";
 const WeekHeader = () => {
   return (
     <>
-      {WeekDetails.map((day) => {
+      {WeekDetails.map((day,dayVal) => {
         return (
-          <th className="react-full-year-calendar-week-name" key={day.key}>
+          <th className={`react-full-year-calendar-week-name ${[0, 6].includes(dayVal % 7) ? "weekend" : ""}`} key={day.key}>
             {day.label}
           </th>
         );
