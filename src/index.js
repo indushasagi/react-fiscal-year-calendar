f80eb206dee3e5146c4d658ddf2e7c3cacf158ae
indushasagi <indusha.sagi@gmail.com>
2021-04-12 16:17:45 +0530

    deploy for vercel

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import FiscalCalendar from './components/FiscalCalendar';
 
-export default FiscalCalendar;
-// ReactDOM.render(
-//   <React.StrictMode>
+//export default FiscalCalendar;
+ReactDOM.render(
+  <React.StrictMode>
     
-//     <FiscalCalendar year={2018} quarterDays={
-//       [
-//         {
-//           "Q_ID":"1",
-//           "ST_DT":"2018-09-23",
-//           "ED_DT":"2018-12-23"
-//         }, // [{month, monthIndex, year, startDay, endDay}, {}, {}]
-//         {
-//           "Q_ID":"2",
-//           "ST_DT":"2018-12-24",
-//           "ED_DT":"2019-03-24"
-//         },
-//         {
-//           "Q_ID":"3",
-//           "ST_DT":"2019-03-25",
-//           "ED_DT":"2019-06-25"
-//         },
-//         {
-//           "Q_ID":"4",
-//           "ST_DT":"2019-06-26",
-//           "ED_DT":"2019-09-22"
-//         }
-//       ]} />
-//     {/* <FiscalCalendar year={2019} quarterMonth="FEBRUARY" /> */}
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+    {/* <FiscalCalendar year={2018} quarterDays={
+      [
+        {
+          "Q_ID":"1",
+          "ST_DT":"2018-09-23",
+          "ED_DT":"2018-12-23"
+        }, // [{month, monthIndex, year, startDay, endDay}, {}, {}]
+        {
+          "Q_ID":"2",
+          "ST_DT":"2018-12-24",
+          "ED_DT":"2019-03-24"
+        },
+        {
+          "Q_ID":"3",
+          "ST_DT":"2019-03-25",
+          "ED_DT":"2019-06-25"
+        },
+        {
+          "Q_ID":"4",
+          "ST_DT":"2019-06-26",
+          "ED_DT":"2019-09-22"
+        }
+      ]} /> */}
+    <FiscalCalendar year={2021} quarterMonth="FEBRUARY" />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
